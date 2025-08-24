92f5898a07418c9d670c1d5edd85fe33448f2f90
Maykmora <maykmora12@gmail.com>
2025-08-24 11:04:34 -0600

    Menú inicial+ menú programa

--- menú.py
+++ menú.py
@@ -1,75 +1,41 @@
-class Cuenta:
-    def __init__(self, nombre, numero_cuenta, pin, saldo):
-        self.nombre = nombre
-        self.numero_cuenta = numero_cuenta
-        self.__pin = pin
-        self._saldo = saldo
-
-    @property
-    def saldo(self):
-        return self._saldo
-
-    @saldo.setter
-    def saldo(self, nuevo_saldo):
-        if nuevo_saldo > 0:
-            self._saldo = nuevo_saldo
-        else:
-            print("El saldo no puede ser menor o igual a 0")
-
-    def mostrar_info(self):
-        return f"Número de cuenta:{self.numero_cuenta} - Saldo: {self.saldo}"
-
-    def sett_pin(self, pin_ingresado):
-        return self.__pin == pin_ingresado
-
-class Registro:
-    def __init__(self):
-        self.diccionario = {}
-
-    def crear_cuenta(self):
-        nombre = input("Ingrese su nombre: ")
-        numero_cuenta = input("Ingrese el número de cuenta que quiera tener: ")
-        pin = input("Escriba el pin de su cuenta: ")
-        saldo = int(input("Ingrese la cantidad de dinero que va a tener su cuenta: "))
-        self.diccionario[numero_cuenta] = Cuenta(nombre,numero_cuenta, pin, saldo)
-
-    def ingresar_cajero(self):
-        numero_cuenta = input("Ingrese el número de cuenta: ")
-        pin_cuenta = input("Ingrese el pin de la cuenta: ")
-        if numero_cuenta not in self.diccionario:
-            print("Cuenta no encontrada")
-
-        cuenta = self.diccionario[numero_cuenta]
-        if cuenta["pin"] == pin_cuenta:
-            print(f"Bienvenido")
-
-    def deposito(self, cuenta):
-        monto = int(input("Ingrese el monto a depositar: "))
-        cuenta.saldo = cuenta.saldo + monto
-        print("Se ha realizado el deposito")
-
-op1=Registro()
-
 while True:
-    print("\n--MENÚ--")
-    print("1.")
-    print("2.")
-    print("3.")
-    print("4.")
-    print("5.Salir\n")
-    option=input("Seleccione una opción (1-5):")
+    print("\n--BIENVENIDO--")
+    print("1.Iniciar sesión")
+    print("2.Crear cuenta")
+    print("3.Salir")
+    option=input("Seleccione una opción (1-3): ")
 
     match option:
         case "1":
-            op1.crear_cuenta()
+
+            while True:
+                print("\n--MENÚ--")
+                print("1.Consultar cuenta")
+                print("2.Retirar")
+                print("3.Cambiar PIN")
+                print("4.Historial de Transacciones")
+                print("5.Salir\n")
+                option2 = input("Seleccione una opción (1-5):")
+
+                match option2:
+                    case "1":
+                        print()
+                    case "2":
+                        print()
+                    case "3":
+                        print()
+                    case "4":
+                        print()
+                    case "5":
+                        print("Gracias por usar el programa.")
+                        break
+                    case _:
+                        print("Ocurrió un error inesperado")
         case "2":
-            op1.deposito()
+            print()
         case "3":
-            print()
-        case "4":
-            print()
-        case "5":
-            print("Gracias por usar el programa.")
+            print("Gracias por usar el programa")
             break
         case _:
-            print("Ocurrió un error inesperado, inténtelo de nuevo.")
+            print("Error inesperado, inténtelo de nuevo")
+
